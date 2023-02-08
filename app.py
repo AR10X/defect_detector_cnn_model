0c2383136a58d50562b205c492ee552ee3ef9cd2
AR10X <arehamanofficial@gmail.com>
2023-02-08 18:18:59 +0530

    Add Image display on prediction

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import io
 from PIL import Image
 import cv2
+import base64
 
 
 app = Flask(__name__)
@@ -17,7 +18,7 @@ def index():
     if request.method == "POST":
         # Get the image from the request
         ImageFile = request.files.get('image').read()
-        
+        image_base64 = base64.b64encode(ImageFile).decode("utf-8")
         # Resize the image to 300x300
         image = Image.open(io.BytesIO(ImageFile))
         image = image.resize((300, 300))
@@ -37,11 +38,11 @@ def index():
         # prediction = str(predictions)
         # Check if the prediction is "def" or "ok"
         if predictions[0][0] < 0.5:
-            prediction = "Given Image is defect"
+            prediction = "DEFECT"
         else:
-            prediction = "Given Image is ok"
+            prediction = "OK"
         
-        return render_template('index.html', prediction=prediction)
+        return render_template('index.html', prediction=prediction, image_base64=image_base64)
     return render_template("index.html")
 
 if __name__ == '__main__':
