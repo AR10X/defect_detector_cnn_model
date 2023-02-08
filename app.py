a0f08c6451b5ffdd8ea47be07a1f9602bfdcf142
AR10X <arehamanofficial@gmail.com>
2023-02-08 18:09:17 +0530

    Updated merged index and predict route to single

--- app.py
+++ app.py
@@ -12,39 +12,37 @@ app = Flask(__name__)
 # Load the model
 model = load_model('defect_detection_pp_woven.h5')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/', methods=['POST', 'GET'])
-def predict():
-    # Get the image from the request
-    image = request.files.get('image').read()
-    
-    # Resize the image to 300x300
-    image = Image.open(io.BytesIO(image))
-    image = image.resize((300, 300))
+def index():
+    if request.method == "POST":
+        # Get the image from the request
+        ImageFile = request.files.get('image').read()
+        
+        # Resize the image to 300x300
+        image = Image.open(io.BytesIO(ImageFile))
+        image = image.resize((300, 300))
 
-    # Convert the image to grayscale
-    image = image.convert('L')
+        # Convert the image to grayscale
+        image = image.convert('L')
 
-    # Convert the image to a numpy array
-    image = np.array(image)
+        # Convert the image to a numpy array
+        image = np.array(image)
 
-    # Add a batch dimension
-    image = np.expand_dims(image, axis=0)
-    image = np.expand_dims(image, axis=-1)
-    # Make predictions with the model
-    predictions = model.predict(image)
+        # Add a batch dimension
+        image = np.expand_dims(image, axis=0)
+        image = np.expand_dims(image, axis=-1)
+        # Make predictions with the model
+        predictions = model.predict(image)
 
-    # prediction = str(predictions)
-    # Check if the prediction is "def" or "ok"
-    if predictions[0][0] < 0.5:
-        prediction = "defect"
-    else:
-        prediction = "ok"
-    
-    return render_template('predict.html', prediction=prediction)
+        # prediction = str(predictions)
+        # Check if the prediction is "def" or "ok"
+        if predictions[0][0] < 0.5:
+            prediction = "Given Image is defect"
+        else:
+            prediction = "Given Image is ok"
+        
+        return render_template('index.html', prediction=prediction)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
