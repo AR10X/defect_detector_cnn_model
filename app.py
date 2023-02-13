8431db1997ffd3e5caea3827a8bffc324952210b
AR10X <arehamanofficial@gmail.com>
2023-02-13 13:23:59 +0530

    Integrate gunicorn

--- app.py
+++ app.py
@@ -12,53 +12,60 @@ import seaborn as sns
 from sklearn.metrics import classification_report, confusion_matrix
 
 
-app = Flask(__name__, static_url_path='/static')
-
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
+import os
 
 
-model = load_model('defect_detection_pp_woven.h5')
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+
+    @app.route('/static/<path:path>')
+    def send_static(path):
+        return send_from_directory('static', path)
+
+
+    model = load_model('defect_detection_pp_woven.h5')
 
 
 
-@app.route('/metrics', methods=['GET'])
-def metrics():
-    return render_template("metrics.html")
+    @app.route('/metrics', methods=['GET'])
+    def metrics():
+        return render_template("metrics.html")
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    version = int(time.time())
-    if request.method == "POST":
-        
-        ImageFile = request.files.get('image').read()
-        image_base64 = base64.b64encode(ImageFile).decode("utf-8")
-        
-        image = Image.open(io.BytesIO(ImageFile))
-        image = image.resize((300, 300))
+    @app.route('/', methods=['POST', 'GET'])
+    def index():
+        version = int(time.time())
+        if request.method == "POST":
 
-        
-        image = image.convert('L')
+            ImageFile = request.files.get('image').read()
+            image_base64 = base64.b64encode(ImageFile).decode("utf-8")
 
-        
-        image = np.array(image)
+            image = Image.open(io.BytesIO(ImageFile))
+            image = image.resize((300, 300))
 
-        
-        image = np.expand_dims(image, axis=0)
-        image = np.expand_dims(image, axis=-1)
-        
-        predictions = model.predict(image)
-        
-        
-        if predictions[0][0] < 0.5:
-            prediction = "DEFECT"
-        else:
-            prediction = "OK"
-        
-        return render_template('index.html', prediction=prediction, image_base64=image_base64, version=version)
-    return render_template("index.html", version=version)
+
+            image = image.convert('L')
+
+
+            image = np.array(image)
+
+
+            image = np.expand_dims(image, axis=0)
+            image = np.expand_dims(image, axis=-1)
+
+            predictions = model.predict(image)
+
+
+            if predictions[0][0] < 0.5:
+                prediction = "DEFECT"
+            else:
+                prediction = "OK"
+
+            return render_template('index.html', prediction=prediction, image_base64=image_base64, version=version)
+        return render_template("index.html", version=version)
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
