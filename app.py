46a2b267644c68c80dda3bc4494eed65da853c23
AR10X <arehamanofficial@gmail.com>
2023-02-15 20:28:19 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import base64
 import time
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__, static_url_path='/static')
 
     @app.route('/static/<path:path>')
@@ -60,5 +60,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    # app.run(host='0.0.0.0', port=5000)
-    app.run()
+    app.run(debug=True)
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
 workers = 4
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:5000"
 chdir = "./"
 module = "app:create_app()"
